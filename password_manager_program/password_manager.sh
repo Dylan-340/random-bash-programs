e1f7ee7c30f3b9cfaa27ae6639eaeee8e1dfca3e
Dylan-340 <dylansb1126@gmail.com>
2026-03-10 18:35:30 -0400

    Update password_manager.sh

--- password_manager_program/password_manager.sh
+++ password_manager_program/password_manager.sh
@@ -6,49 +6,58 @@ clear
 encrypt_password(){
     echo "Input the password to be encrypted"
     read -s password
-    hex_password=$(printf '%s' "$password" | xxd -p)
-
+    hex_password=$(printf "%s" "$password" | xxd -p)
 
     echo "Input the encryption hex number (WRITE THIS DOWN)"
     read -s key
 
-    #hex_output=$(printf '%s' "$password" | xxd -p)
 
-    encrypted_password=$((0x$hex_password * 0x$key))    
-    #echo $encrypted_password
-    printf "%x" "$encrypted_password"
+    encrypted_password=$((0x$hex_password * 0x$key))   
+
+    printf "%x\n" "$encrypted_password"
 
     echo "$encrypted_password" >> password.txt
+
+    echo "Encrypted password stored to file password.txt"
 
 }
 
 decrypt_passwords(){
+    echo "Input the password to be decrypted"
+    read password
+
     echo "Please input the decryption key to see your passwords."
     read -s key
 
-    encrypted_passwords=$(cat password.txt)
+    decrypted_password=$(( password / 0x$key ))
     
-    decrypted_passwords=$((0x$encrypted_passwords / 0x$key))
-
-    printf "%x" "$decrypted_passwords"
+    hex=$(printf "%x" "$decrypted_password")
+    echo "Decrypted hex value: $hex"
+    echo -n "Decrypted password: "
+    echo "$hex" | xxd -r -p 
+    echo ""
 }
 
 
 
-echo "Password Manager
+echo "Password Manager (ctrl+c to exit)
 "
-read -p "Would you like to encrypt or decrypt a password?[e/d]: " mode
+
 
 input_loop=0
 
-while [ input_loop != 1 ] do 
+while [ input_loop != 1 ]; do
 
-if [[ $mode == "e" || $mode == "E" ]]; then
-    encrypt_password
-elif [[ $mode == "d" || $mode == "D" ]]; then
-    decrypt_passwords
-else
-    echo "not a valid answer"
-fi
+    read -p "Would you like to encrypt or decrypt a password?[e/d]: " mode
+
+    if [[ $mode == "e" || $mode == "E" ]]; then
+        encrypt_password
+        input_loop=1
+    elif [[ $mode == "d" || $mode == "D" ]]; then
+        decrypt_passwords
+        input_loop=1
+    else
+        echo "not a valid answer"    
+    fi
 
 done
